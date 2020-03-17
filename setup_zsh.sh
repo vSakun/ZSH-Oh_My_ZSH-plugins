876762e07f8ae776018f0cb4ac6cdf1ff1739bb7
Vitali Sakun <vsakun@gmail.com>
2020-03-17 12:11:11 +0300

    + kali

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -2,8 +2,7 @@
 
 read -s -p "Enter your password: " pass
 #echo -e "\n Is your password really $pass? "
-echo $pass >> /dev/null 
-
+echo $pass
 os=$(echo $pass | sudo -S cat /etc/os-release | grep 'ID=' | head -n 1 | cut -d '=' -f2)
 if [[ $os == 'ubuntu' ]] ; then
 	if [ -f "/usr/bin/git*" ] ; then
@@ -13,15 +12,21 @@ if [[ $os == 'ubuntu' ]] ; then
 	fi
 elif [[ $os == 'linuxmint' ]] ; then
 	if [ -f "/usr/bin/git*" ] ; then
-		echo '<===GIT and ZSH INSTALL===>'
+		echo '<===GIT INSTALL===>'
 	else
 		echo $pass | sudo -S apt update && echo $pass | sudo -S apt install fonts-powerline git zsh -y
 	fi
 elif [[ $os == 'fedora' ]] ; then
 	if [ -f "/usr/bin/git*" ] ; then
-		echo '<===GIT and ZSH INSTALL===>'
+		echo '<===GIT INSTALL===>'
 	else
-		echo $pass | sudo -S dnf update -n && echo $pass | sudo -S dnf install git zsh -y 
+		echo $pass | sudo -S dnf update -n && echo $pass | sudo -S dnf install git zsh -y
+	fi
+elif [[ $os == 'kali' ]]; then
+	if [ -f "/usr/bin/git*" ] ; then
+		echo '<===GIT INSTALL===>'
+	else
+		echo $pass | sudo -S apt update && echo $pass | sudo -S apt install fonts-powerline git zsh -y
 	fi
 fi
 #read -s -p "Enter your password: "
@@ -33,7 +38,7 @@ elif [ -f "/usr/bin/wget" ] ; then
 	echo '<===OH-MY-ZSH DOWNLOAD WGET ADN INSTALL===>'
 	echo Y | sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 else
-	if [[ $os == 'ubuntu' || $os == 'linuxmint' ]] ; then
+	if [[ $os == 'ubuntu' || $os == 'linuxmint' || $os == 'kali' ]] ; then
 		echo '<===INSTALL CURL DEBIAN===>' 
 		echo $pass | sudo -S apt install curl -y
 		echo Y | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -44,13 +49,35 @@ else
 	fi
 fi
 
-echo '<==== install zsh-syntax-highlighting and zsh-autosuggestions ====>'
+echo '<==== install zsh-syntax-highlighting and  ====>'
 echo $pass | sudo -S git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo $pass | sudo -S git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sed -i '/ZSH_THEME/s/robbyrussell/agnoster/' ~/.zshrc
 sed -i '/plugins/s/git/git\ docker\ docker-compose\ zsh-syntax-highlighting\ zsh-autosuggestions/' ~/.zshrc
-echo $pass | sudo -S chsh -s $(which zsh) $USER
-source ~/.zshrc
 zsh
+echo $pass | sudo -S chsh -s $(which zsh)
+source ~/.zshrc
+
+
+
+
+
+#!/usr/bin/expect --
+
+#set pass "yourpass"
+
+#proc setpass { pass } {
+#    spawn your_mega_cli_cmd # Ваша программа с параметрами тут
+#    expect_after {
+#        timeout { catch { close }; wait; return 1 }
+#        eof { catch { close }; wait; return 1 }
+#    }
+#    expect {
+#        ".*assword.*" {send  "$pass\r"; exp_continue}
+#    }
+#    expect eof
+#}
+
+#setpass $pass
